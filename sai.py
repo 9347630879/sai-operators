97289d0d7cadc95479046b0db4d7d6053e7fd5b6
9347630879 <145848207+9347630879@users.noreply.github.com>
2023-09-30 15:59:40 +0530

    Add files via upload

---
+++ sai.py
@@ -0,0 +1,75 @@
+#left shift operator
+print( 8>>3)
+
+print( 9>>5)
+
+print( 20>>10)
+
+#right left operator
+
+print( 8<<2)
+
+print( 4<<7)
+
+print( 10<<3)
+
+
+#check address of  a variable
+
+name = "dhanunjai sai"
+print(id(name))
+
+a = 25
+print(id(a))
+
+name = "sundhar"
+print(id(name))
+
+#identify operator
+#is
+a = 12
+b = 11
+print(a is b)
+
+
+a = 50
+b = 50
+print(a is b)
+
+#is not
+
+a = 12
+b = 11
+print(a is not b)
+
+a = 106
+b = 100
+print(a is b)
+
+#membership operator
+#in
+
+a = "gokavaramsai@gmail.com"
+print("@" in a)
+
+a = "gokavaramsai$gmail.com"
+print("$" in a)
+
+attendances = ( "dhanunnjai", "sai", "sundhar")
+print( "sai" in attendances )
+
+attendances = ( "dhanunnjai", "sai", "sundhar")
+a = "sweety"
+print( "sweety" in attendances )
+
+
+#notin
+
+a = "gokavaramsai@gmail.com"
+print("@" not in a )
+
+
+a = "gokavaramsai&gmail.com"
+print("&" not in a )
+
+
